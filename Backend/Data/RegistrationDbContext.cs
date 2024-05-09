91dd1d64e15a2f9023adc22c545c88ab7a06c076
Kumar Sunny <kumarsunny1348@gmail.com>
2024-05-09 09:40:22 +0200

    Create project structure and models

---
+++ Backend/Data/RegistrationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using newRegistrationApp.Models.Domain;
+
+namespace newRegistrationApp.Data
+{
+    public class RegistrationDbContext : DbContext
+    {
+        public RegistrationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
+
---
+++ Backend/Models/Domain/Company.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace newRegistrationApp.Models.Domain
+{
+	public class Company
+	{
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Industry { get; set; }
+    }
+}
+
