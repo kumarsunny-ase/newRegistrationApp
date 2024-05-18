423432dd08736cbd50aa1eb6f07a0dcbd03d138a
Kumar Sunny <kumarsunny1348@gmail.com>
2024-05-18 12:46:59 +0200

    make changes in Styling

--- Frontend/src/app/services/registration/registration.service.ts
+++ Frontend/src/app/services/registration/registration.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -11,21 +11,21 @@ export class RegistrationService {
   // Fetch industries from the server
   getIndustries(): Observable<string[]> {
     return this.http.get<string[]>(
-      'https://localhost:7019/api/Registration/industries'
+      'http://localhost:5276/api/Registration/industries'
     );
   }
 
   // Check if the provided username already exists
   checkUserName(username: string): Observable<boolean> {
     return this.http.get<boolean>(
-      `https://localhost:7019/api/Registration/userName?username=${username}`
+      `http://localhost:5276/api/Registration/userName?username=${username}`
     );
   }
 
   // Submit user registration data to the server
   submitData(formData: any) {
     return this.http.post<any>(
-      'https://localhost:7019/api/Registration/summary',
+      'http://localhost:5276/api/Registration/summary',
       formData
     );
   }
